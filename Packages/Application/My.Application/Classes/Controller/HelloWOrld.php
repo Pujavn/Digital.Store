ad3295c690cef9c87cbb653f45f53b39018ec40f
sumit0208 <sumithalgekar@gmail.com>
2017-06-27 16:22:05 +0530

    Created the package

---
+++ Packages/Application/My.Application/Classes/Controller/HelloWOrld.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace My\Application\Classes\Controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of HelloWOrld
+ *
+ * @author sumit
+ */
+class HelloWOrld {
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		var_dump('Yipee! I am working :)');
+		exit;
+	}
+
+}
